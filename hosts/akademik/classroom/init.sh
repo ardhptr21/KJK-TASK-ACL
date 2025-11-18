adef5f1f7f21b6d8a56783e52b6d316d5ce41147
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-11-19 00:14:32 +0700

    add http ssh simulation acl

--- hosts/akademik/classroom/init.sh
+++ hosts/akademik/classroom/init.sh
@@ -1,8 +1,13 @@
 grep -qxF 'nameserver 192.168.122.1' /etc/resolv.conf || echo 'nameserver 192.168.122.1' >> /etc/resolv.conf
 
 apt update
-apt install nginx -y
+apt install nginx openssh-server -y
 
+# SETUP WEB SERVER
 service nginx restart
-
 echo "<h1>Classroom Server</h1>" > /var/www/html/index.html
+
+# SETUP SSH SERVER
+useradd -m -s /bin/bash classroom
+echo "classroom:123" | chpasswd
+service ssh restart
--- routers/edge/init.sh
+++ routers/edge/init.sh
@@ -5,5 +5,8 @@ apt install iptables -y
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 10.20.0.0/16
 
 # [=== FIREWALL RULES ===]
-iptables -A FORWARD -i eth1 -d 10.20.20.2 -p tcp --dport 80 -j DROP
-iptables -A OUTPUT -s 10.20.30.0/24 -d 10.20.20.2 -p tcp --dport 80 -j DROP
+iptables -A FORWARD -s 10.20.50.0/24 -d 10.20.20.2 -p tcp --dport 80 -j REJECT # reject guest    -> akademik classroom HTTP
+iptables -A FORWARD -s 10.20.30.0/24 -d 10.20.20.2 -p tcp --dport 80 -j REJECT # reject risetiot -> akademik classroom HTTP
+
+iptables -A FORWARD -s 10.20.40.0/24 -d 10.20.20.0/24 -p tcp --dport 22 -j ACCEPT # allow admin -> akademik all ssh
+iptables -A FORWARD -d 10.20.20.2 -p tcp --dport 22 -j REJECT                     # reject all  -> akademik all ssh
