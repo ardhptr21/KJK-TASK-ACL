c9e7980574fda6b0a658ac2965b58feeb991899d
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-12-03 13:31:36 +0700

    setup suricata ips/ids

--- routers/edge/init.sh
+++ routers/edge/init.sh
@@ -19,3 +19,20 @@ iptables -A FORWARD -s 10.20.40.0/24 -d 10.20.20.0/24 -p tcp --dport 22 -j ACCEP
 iptables -A FORWARD -d 10.20.20.0/24 -p tcp --dport 22 -j REJECT                  # reject all  -> akademik all ssh
 
 iptables -A FORWARD -d 10.20.20.4 -p tcp --dport 3306 -j REJECT # reject all
+
+
+# IDS & IPS Configuration
+apt install software-properties-common -y
+add-apt-repository ppa:oisf/suricata-stable -y
+apt update
+apt install suricata -y
+
+suricata-update
+# suricata -c /etc/suricata/suricata.yaml -i eth0 -i eth1 -i eth2 -i eth3 -i eth4 -i eth5 -D -> af-packets mode
+# suricata -c /etc/suricata/suricata.yaml -q 0 -D -> nfqeueue mode
+
+# =CUSTOM=
+# edit /etc/suricata/suricata.yaml
+# add new file rules -> local.rules
+
+echo 'alert http any any -> any any (msg:"WEB SHELL / BACKDOOR access detected"; flow:established,to_server; uricontent:".php"; nocase; pcre:"/((cmd|shell|upload|eval|exec|system|backdoor|cmd\.php|shell\.php|uploader|webshell)\.php)/i"; classtype:web-application-attack; sid:1002001; rev:1;)' >> /var/lib/suricata/rules/local.rules
